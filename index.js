836a706d5ccbe1d7fb0bed6e86aba0be3f74675e
Gab <gabcinder2004@yahoo.com>
2021-09-17 23:17:11 -0400

    Adjusted proj pts

--- index.js
+++ index.js
@@ -10,36 +10,34 @@ const port = process.env.PORT || 3001;
 
 app.get("/getTeams", async (req, res) => {
   try {
-    let url = `https://dfyql-ro.sports.yahoo.com/v2/contestEntries?lang=en-US&region=US&device=desktop&sort=rank&contestId=${constants.YAHOO_CONTEST_ID}&start=0&limit=20`
-    let response = await axios.get(url
-      ,
-      {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers":
-            "Origin, X-Requested-With, Content-Type, Accept",
-          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyNSwiaWF0IjoxNjIwMzUzNzgzLCJleHAiOjE2MjAzNjQ1ODMsInN1YiI6ImdhYiJ9.wd3FFje8CYxrCp00vELgGRRNBJNRWhKiKuDo5GWmcZo",
-        },
-      }
-    );
+    let url = `https://dfyql-ro.sports.yahoo.com/v2/contestEntries?lang=en-US&region=US&device=desktop&sort=rank&contestId=${constants.YAHOO_CONTEST_ID}&start=0&limit=20`;
+    let response = await axios.get(url, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept",
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyX2lkIjoyNSwiaWF0IjoxNjIwMzUzNzgzLCJleHAiOjE2MjAzNjQ1ODMsInN1YiI6ImdhYiJ9.wd3FFje8CYxrCp00vELgGRRNBJNRWhKiKuDo5GWmcZo",
+      },
+    });
     if (response.status !== 200) {
       throw `Status not 200. Status is ${response.status}`;
     }
 
     var players = [];
-    response.data.entries.result.forEach(player => {
-        players.push({
-            id: player.id,
-            name: player.user.nickname,
-            imageThumbUrl: player.user.imageThumbUrl,
-            seriesId: player.seriesId,
-            periodsRemaining: player.periodsRemaining,
-            percentile: player.percentile,
-            rank: player.rank,
-            remainingTimeUnit: player.remainingTimeUnit,
-            score: player.score
-        })
-    });;
+    response.data.entries.result.forEach((player) => {
+      players.push({
+        id: player.id,
+        name: player.user.nickname,
+        imageThumbUrl: player.user.imageThumbUrl,
+        seriesId: player.seriesId,
+        periodsRemaining: player.periodsRemaining,
+        percentile: player.percentile,
+        rank: player.rank,
+        remainingTimeUnit: player.remainingTimeUnit,
+        score: player.score,
+      });
+    });
     res.status(200).send(players);
   } catch (err) {
     res.status(500).send({ err });
@@ -47,44 +45,52 @@ app.get("/getTeams", async (req, res) => {
 });
 
 app.get("/getLineupForTeam", async (req, res) => {
-    try {
-      const url = `https://dfyql-ro.sports.yahoo.com/v2/contestEntry/${req.query.id}`;
+  try {
+    const url = `https://dfyql-ro.sports.yahoo.com/v2/contestEntry/${req.query.id}`;
 
-      let response = await axios.get(url
-        ,
-        {
-          headers: {
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Headers":
-              "Origin, X-Requested-With, Content-Type, Accept"          },
-        }
-      );
-      if (response.status !== 200) {
-        throw `Status not 200. Status is ${response.status}`;
-      }
-  
-      var players = [];
-      response.data.entries.result[0].lineupSlotList.forEach(lineup => {
-          players.push({
-            position: lineup.lineupSlot.key,
-            firstName: lineup?.player?.firstName ?? "",
-            lastName: lineup?.player?.lastName ?? "",
-            salary: lineup?.player?.salary ?? "",
-            primaryPosition: lineup?.player?.primaryPosition ?? "",
-            projectedPoints: lineup?.player?.projectedPoints ?? "",
-            points: lineup?.player?.points ?? "",
-            draftPercent: lineup?.player?.playerDraftPercent ?? "",
-            image: lineup?.player?.largeImageUrl ?? "",
-            stats: lineup?.player?.stats,
-            gameStatusType: lineup?.player?.game?.statusType,
-            gameStatus: lineup?.player?.game?.status
-          });
-      });;
-      res.status(200).send(players);
-    } catch (err) {
-      res.status(500).send({err: err.message, stack: err.stack });
+    let response = await axios.get(url, {
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept",
+      },
+    });
+
+    if (response.status !== 200) {
+      throw `Status not 200. Status is ${response.status}`;
     }
-  });
+
+    var players = [];
+    response.data.entries.result[0].lineupSlotList.forEach((lineup) => {
+      let projPts = lineup?.player?.projectedPoints ?? "";
+      let adjustedProj = projPts;
+      if (projPts != "") {
+        let gameRemainingTime = lineup?.player?.game?.remainingTimeUnit;
+        let projPtsPerMin = projPts / 60;
+        adjustedProj = (projPtsPerMin * gameRemainingTime) + lineup?.player?.points;
+      }
+
+      players.push({
+        position: lineup.lineupSlot.key,
+        firstName: lineup?.player?.firstName ?? "",
+        lastName: lineup?.player?.lastName ?? "",
+        salary: lineup?.player?.salary ?? "",
+        primaryPosition: lineup?.player?.primaryPosition ?? "",
+        projectedPoints: adjustedProj,
+        points: lineup?.player?.points ?? "",
+        draftPercent: lineup?.player?.playerDraftPercent ?? "",
+        image: lineup?.player?.largeImageUrl ?? "",
+        stats: lineup?.player?.stats,
+        gameStatusType: lineup?.player?.game?.statusType,
+        gameStatus: lineup?.player?.game?.status,
+
+      });
+    });
+    res.status(200).send(players);
+  } catch (err) {
+    res.status(500).send({ err: err.message, stack: err.stack });
+  }
+});
 
 app.listen(port, () => {
   console.log(`DFS Server listening on ${port}`);
