a62a6a3a45e2e12d2aa81de653304c01d9191873
Gab <gabcinder2004@yahoo.com>
2021-10-10 23:43:08 -0400

    null reference

--- index.js
+++ index.js
@@ -72,7 +72,7 @@ app.get("/getLineupForTeam", async (req, res) => {
 
       if (lineup?.player?.game?.finished == false) {
         var test = 1;
-        if (pointsScored != "" && this.lastUpdate_players[req.query.id].length > 0) {
+        if (pointsScored != "" && this.lastUpdate_players.players[req.query.id] && this.lastUpdate_players[req.query.id].length > 0) {
           let lastUpdate_player = this.lastUpdate_players[req.query.id].find(
             (x) => x.id == lineup.player.playerSalaryId
           );
