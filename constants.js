150ff1a9a031d81f42a188c716e9e0a13a6e8d39
Gabriel Nunez <gabriel_nunez@ukg.com>
2022-10-01 16:42:24 -0400

    Week 4

--- constants.js
+++ constants.js
@@ -1,8 +1,9 @@
 module.exports = {
-    YAHOO_CONTEST_ID: 11340116,
-    week: 3,
+    YAHOO_CONTEST_ID: 11379609,
+    week: 4,
     year: 2022,
 }
 
 //WEEK1: 11219805
 //WEEK2: 11295164
+//WEEK3: 11340116
